d694262c064ecc725f375cf3c04aa8c1d010a718
heyywillz <atabisawilliams@gmail.com>
2025-03-31 22:54:42 +0000

    Making the navigation responsive

---
+++ new-homepage/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleBtn = document.getElementById("toggle-btn");
+    const navLinks = document.getElementById("nav-links");
+
+    toggleBtn.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+    });
+});
