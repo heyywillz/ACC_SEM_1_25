f08747dc05f0efb923bd7aa7684bd8feff3634fa
heyywillz <atabisawilliams@gmail.com>
2025-04-01 01:03:19 +0000

    Modification on the navigation

--- new-homepage/script.js
+++ new-homepage/script.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const toggleBtn = document.getElementById("toggle-btn");
     const navLinks = document.getElementById("nav-links");
+    const bodyShade = document.getElementById("bodyShade");
 
     toggleBtn.addEventListener("click", function () {
         navLinks.classList.toggle("active");
+        bodyShade.classList.toggle("active");
+
     });
 });
